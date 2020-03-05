bf96eac9bbd5eef55a0ceb486e1c58282eae5d63
LawssssCat <1191693505@qq.com>
2020-03-05 23:20:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <tabbar>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import TabBar from './components/tabbar/TabBar'
+export default {
+  name: 'App',
+  data () {
+    return {
+      items: ['首页', '分类', '购物车', '我的']
+    }
+  },
+  components: {
+    TabBar
+  }
+}
+</script>
+
+<style>
+@import url("./assets/css/base.css");
+</style>
---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <div id="tab-bar"></div>
+  <div v-for="(item, index) in items" :key="index" class="tab-bar-item">{{ item }}</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TabBar'
+}
+</script>
+
+<style>
+#tab-bar{
+  display: flex;
+  text-align: center;
+  height: 49px;
+
+  /*到最小面*/
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  box-shadow: 0px -3px 1px rgba(100, 100, 100, .09);
+}
+
+.tab-bar-item {
+  flex: 1;
+  background-color: #ecf0f1;
+}
+</style>
