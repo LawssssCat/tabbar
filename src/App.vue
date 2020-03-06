9916de6ef7c0a7c53fc1cd38a7aed25c32340822
LawssssCat <1191693505@qq.com>
2020-03-06 14:45:20 +0800

    修复：图片不显示（静态路径问题）

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     <tab-bar :items='tabBarItems'></tab-bar>
+
   </div>
 </template>
 
 <script>
 import TabBar from './components/tabbar/TabBar'
+
+let home = require('./assets/img/keai.gif')
+console.log(home)
 
 export default {
   name: 'App',
@@ -14,23 +18,23 @@ export default {
       tabBarItems: [
         {
           name: '首页',
-          img: '/assets/img/tabbar/home.svg',
-          activeImg: '../../assets/img/tabbar/home_active.svg'
+          img: '/static/img/tabbar/home.svg',
+          activeImg: '/static/img/tabbar/home_active.svg'
         },
         {
           name: '分类',
-          img: '../../assets/img/tabbar/category.svg',
-          activeImg: '../../assets/img/tabbar/category_active.svg'
+          img: '/static/img/tabbar/category.svg',
+          activeImg: '/static/img/tabbar/category_active.svg'
         },
         {
           name: '购物车',
-          img: '../../assets/img/tabbar/cart.svg',
-          activeImg: '../../assets/img/tabbar/cart_active.svg'
+          img: '/static/img/tabbar/cart.svg',
+          activeImg: '/static/img/tabbar/cart_active.svg'
         },
         {
           name: '我的',
-          img: '../../assets/img/tabbar/profile.svg',
-          activeImg: '../../assets/img/tabbar/profile_active.svg'
+          img: './static/img/tabbar/keai.gif',
+          activeImg: '/static/img/tabbar/keai.gif'
         }
       ]
     }
